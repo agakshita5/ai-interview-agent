b2c534d918ef5ee49964efd10ca462ecd6bdf17a
Akshita Agarwal <akshita.ag2020@gmail.com>
2025-08-11 10:54:23 +0530

    feat: added llm and tts

---
+++ voice-to-voice AI assistant/record_transcription.py
@@ -0,0 +1,99 @@
+'''recording audio'''
+# python-sounddevice records to NumPy arrays and pyaudio records to bytes objects. 
+# Both of these can be stored as WAV files using the scipy and wave libraries, respectively
+
+import sounddevice as sd
+from scipy.io.wavfile import write
+
+fs = 44100  # Sample rate
+seconds = 5  # Duration of recording
+
+myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
+print("Recording...")
+sd.wait()  # Wait until recording is finished
+print("Recording finished")
+# audio_data = np.copy((myrecording * 32767).astype(np.int16)) -> when using simplaaudio
+write('/Users/agakshita/AI/voice-to-voice-ai-assistant/record-output.wav', fs, myrecording) 
+print("WAV file saved")
+# 32767 is the maximum value for a 16-bit integer
+# The default behavior of sounddevice.rec() with scipy.io.wavfile.write() saves the audio as float32, which is not compatible with simpleaudio
+# hence converting the float recording (in range -1.0 to 1.0) to 16-bit PCM format — the standard for WAV files.
+
+'''either convert wav to mp3 or mp3 to wav''' 
+# can play either of them -> use pygame, it worked for both wav and mp3
+
+'''playing wav file'''
+
+# import simpleaudio as sa -> not working for wav
+# from pydub import AudioSegment -> not working for wav
+# playsound -> outdatd on macOs
+
+# from pygame import mixer
+# import time
+# mixer.init()
+# mixer.music.load('/Users/agakshita/AI/voice-to-voice-ai-assistant/record-output.wav')
+# mixer.music.play()
+# while mixer.music.get_busy(): # wait for music to finish playing
+#     time.sleep(1)
+
+'''converting wav to mp3 and can play it using pygame'''
+# from pydub import AudioSegment
+# sound = AudioSegment.from_wav('/Users/agakshita/AI/voice-to-voice-ai-assistant/record-output.wav')
+# sound.export('/Users/agakshita/AI/voice-to-voice-ai-assistant/record-output.mp3', format='mp3')
+
+'''
+task pr for each
+master branch code
+task list (to-do)
+to do (in progress)
+new task new pr/branch - branch nm = task nm
+pr (merge)
+new task
+'''
+
+'''transcibing audio using whisper'''
+import whisper
+
+model = whisper.load_model("turbo")
+user_input = model.transcribe("record-output.wav")
+user_prompt = user_input["text"]
+
+'''ai reasoning'''
+
+from groq import Groq
+import os
+
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+def ask_groq(prompt):
+    completion = client.chat.completions.create(
+        model="openai/gpt-oss-120b", 
+        messages=[
+            {"role": "user", "content": prompt}
+        ],
+        temperature=1,
+        max_completion_tokens=200,  
+        top_p=1,
+        reasoning_effort="medium",
+        stream=False
+    )
+    return completion.choices[0].message.content
+
+assistant_reply = ask_groq(user_prompt)
+
+
+'''text -> speech'''
+
+from gtts import gTTS
+
+tts = gTTS(assistant_reply)
+tts.save("/Users/agakshita/AI/voice-to-voice-ai-assistant/response.mp3")
+
+'''playing response'''
+from pygame import mixer
+import time
+mixer.init()
+mixer.music.load('/Users/agakshita/AI/voice-to-voice-ai-assistant/response.mp3')
+mixer.music.play()
+while mixer.music.get_busy(): 
+    time.sleep(1)
