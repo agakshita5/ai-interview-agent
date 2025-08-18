c7abef8c96cb3f94383cae93efa568eb6ec14394
agakshita5 <akshita.ag2020@gmail.com>
2025-08-18 08:14:08 +0530

    Remove tempCodeRunnerFile.py from tracking

--- src/api_integration/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-import json
-def get_question_set(interview_id: int):
-    with open("data/questions.json", "r") as f:
-        return json.load(f)[interview_id-1]
-
-print(get_question_set(2))
--- src/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-from fastapi import FastAPI
-app = FastAPI()
-@app.get("/")
-def first_example():
-    return {"hello": "FastAPI"}
