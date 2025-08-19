49c130cd3944829dbd1e75992fa3cd98ae4d0c35
agakshita5 <akshita.ag2020@gmail.com>
2025-08-19 19:34:34 +0530

    updated code

--- src/api_integration/api_client.py
+++ src/api_integration/api_client.py
@@ -2,6 +2,3 @@ import json
 def get_question_set(ques_id: int):
     with open("data/questions.json", "r") as f:
         return json.load(f)[ques_id]
-
-for i in range(15):
-    print(get_question_set(i)['topic'])
