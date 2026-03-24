1271767eb4446ab56c604fa5aeda24c92af2c6b2
agakshita5 <akshita.ag2020@gmail.com>
2026-03-24 23:47:42 +0530

    react frontend

---
+++ react_frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function App(){
+    return (
+        <>
+            <header>
+                <nav>
+                    <h1>AI Voice Interview</h1>
+                </nav>
+            </header>
+            <main>
+                <h1>hehe</h1>
+            </main>
+        </>
+    )
+}
